1fc605aed73c363780cf66be82908ad068fc3620
Luigi de Azevedo Lopes <108105420+zyklon1@users.noreply.github.com>
2024-11-14 10:56:39 -0300

    Sprite

--- godot-roguelike-tutorial-main/Characters/Character.gd
+++ godot-roguelike-tutorial-main/Characters/Character.gd
@@ -1,4 +1,4 @@
-@icon("res://Art/v1.1 dungeon crawler 16x16 pixel pack/heroes/knight/knight_idle_anim_f0.png")
+@icon("res://Art/v1.1 dungeon crawler 16x16 pixel pack/heroes/knight/personagem.png")
 
 extends CharacterBody2D
 class_name Character
